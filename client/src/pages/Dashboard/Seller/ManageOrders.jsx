a3ccebd4cd702f54b199ceeecce60e08f5a9aba8
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-15 23:59:42 +0600

    Main JSX File and Signup Component Updated

--- client/src/pages/Dashboard/Seller/ManageOrders.jsx
+++ client/src/pages/Dashboard/Seller/ManageOrders.jsx
@@ -1,6 +1,8 @@
 import { Helmet } from "react-helmet-async";
 import SellerOrderDataRow from "../../../components/Dashboard/TableRows/SellerOrderDataRow";
 
+
+
 const ManageOrders = () => {
   return (
     <>
