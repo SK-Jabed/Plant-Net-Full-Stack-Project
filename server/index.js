973de2e2e09a47a81c634987c194a8808195765b
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-19 23:59:39 +0600

    Index JS Updated

--- server/index.js
+++ server/index.js
@@ -36,7 +36,6 @@ const verifyToken = async (req, res, next) => {
   });
 };
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mq0mae1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -89,7 +88,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Hello from PlantNet Server...");
