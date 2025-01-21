07f015714aeeed034e523667e025a15610b8d67d
Sheikh Jabed <jabedbd2295@gmail.com>
2025-01-21 19:34:12 +0600

    DashBoard Layout JSX File Updated

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -9,7 +9,7 @@ const DashboardLayout = () => {
       {/* Right Side: Dashboard Dynamic Content */}
       <div className="flex-1  md:ml-64">
         <div className="p-5">
-          {/* Outlet for dynamic contents */}
+          {/* Outlet for Dynamic Contents */}
           <Outlet />
         </div>
       </div>
